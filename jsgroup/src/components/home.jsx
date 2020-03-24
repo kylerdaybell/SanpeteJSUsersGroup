5f1a88fe94384e49e3c96a79c31c4913745ba957
Kyler Daybell <kyler.daybell96@gmail.com>
2020-03-24 10:49:30 -0600

    added an image and changed the navbar

--- jsgroup/src/components/home.jsx
+++ jsgroup/src/components/home.jsx
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
-import logo from '../logo.png';
+import logo from '../public/images/logo.png';
+import group from '../public/images/Group.jpg'
 
 class Homepage extends Component{
     render() {
         return (
+            <>
             <div className="App">
                 <img src={logo} className="App-logo" alt="logo" />
             </div>
+
+            <div className="post">
+                <div className="title">
+                    <h1>Welcome to the Sanpete Javascript Users Group</h1>
+                </div>
+                <div className="content">
+                    <p>Founded in 2020</p>
+                    <p>The goal of the SJSUG is to provide Javascript programmers 
+                        in sanpete county with a valuable community.</p>
+                </div>
+                <img className="image" src={group}></img>
+            </div>
+            </>
           );
     }
 }
--- jsgroup/src/components/navbar.jsx
+++ jsgroup/src/components/navbar.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import singlelinelogo from '../public/images/Single-Line-Logo.png'
 class Navbar extends Component {
     render() {
       return (
         <div className="nav">
           <ul>
-            <li><a className="active" href="#">Home</a></li>
+            <li><a className="home" href="#">Sanpete.js</a></li>
             <li><a href="#news">News</a></li>
             <li><a href="#contact">Contact</a></li>
             <li><a href="#about">About</a></li>
