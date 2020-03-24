05c55ecbfcf24ff0c1ee9a4574af0472d98cbd86
Kyler Daybell <kyler.daybell96@gmail.com>
2020-03-24 09:38:42 -0600

    made the navbar standard on all pages

--- jsgroup/src/App.js
+++ jsgroup/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Route, Switch } from "react-router";
 import Homepage from './components/home';
+import Navbar from './components/navbar';
+import './App.css';
 
 const App = () => {
   return (
     <>
+    <Navbar/>
     <div className="container">
       <Switch>
         <Route exact path="/" component={Homepage} />
--- jsgroup/src/components/home.jsx
+++ jsgroup/src/components/home.jsx
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
 import logo from '../logo.png';
-import '../App.css';
-import Navbar from './navbar'
-
 
 class Homepage extends Component{
     render() {
         return (
             <div className="App">
-              <Navbar />
                 <img src={logo} className="App-logo" alt="logo" />
             </div>
           );
