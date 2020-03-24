e0d312c6eeca4de5124e11d7c15c4b8ddc295e8b
Kyler Daybell <kyler.daybell96@gmail.com>
2020-03-24 12:02:36 -0600

    added the first meeting

--- jsgroup/src/components/home.jsx
+++ jsgroup/src/components/home.jsx
@@ -11,13 +11,24 @@ class Homepage extends Component{
             </div>
 
             <div className="post">
-                <div className="title">
+                <div className="title text-seafoam">
+                    <h1>Our First Meeting.</h1>
+                </div>
+                <div className="content">
+                    <h2>The first meeting of Sanpete.js will take place on april 1st 2020 via Skype. 
+                        <br/>we will be covering the following:</h2>
+                        <p>Building API's with Express.JS and MySQL</p>                        
+                </div>
+            </div>
+
+            <div className="post darkgrey text-white">
+                <div className="title text-seafoam">
                     <h1>Welcome to the Sanpete Javascript Users Group</h1>
                 </div>
                 <div className="content">
                     <p>Founded in 2020</p>
-                    <p>The goal of the SJSUG is to provide Javascript programmers 
-                        in sanpete county with a valuable community.</p>
+                    <h2>The goal of the SJSUG is to provide Javascript programmers 
+                        in sanpete county with a valuable community.</h2>
                 </div>
                 <img className="image" src={group}></img>
             </div>
