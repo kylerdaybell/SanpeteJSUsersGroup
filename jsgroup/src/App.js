84b78dd4db582c1a0f399e6284a4cefe1cbc52b6
Kyler Daybell <kyler.daybell96@gmail.com>
2020-03-24 09:43:00 -0600

    there is now a not found page

--- jsgroup/src/App.js
+++ jsgroup/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from "react-router";
 import Homepage from './components/home';
 import Navbar from './components/navbar';
+import Notfound from './components/notfound'
 import './App.css';
 
 const App = () => {
@@ -11,8 +12,8 @@ const App = () => {
     <div className="container">
       <Switch>
         <Route exact path="/" component={Homepage} />
-        {/* <Route path="/404" component={NotFound} />
-        <Route component={NotFound} /> */}
+        <Route path="/404" component={Notfound} />
+        <Route component={Notfound} />
       </Switch>
     </div>
     </>
---
+++ jsgroup/src/components/notfound.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from '../logo.png';
+
+class Notfound extends Component{
+    render() {
+        return (
+            <div className="App">
+                <h1>The page you are looking for does not exist</h1>
+            </div>
+          );
+    }
+}
+export default Notfound;
