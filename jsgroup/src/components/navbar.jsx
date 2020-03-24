7ab729ea37a5d07e8c06ba53a735c6f5c77badb6
Kyler Daybell <kyler.daybell96@gmail.com>
2020-03-24 09:02:02 -0600

    about to add hashrouter

--- jsgroup/src/components/navbar.jsx
+++ jsgroup/src/components/navbar.jsx
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 class Navbar extends Component {
     render() {
       return (
-        <div class="nav">
+        <div className="nav">
           <ul>
-            <li><a class="active" href="/">Home</a></li>
+            <li><a className="active" href="#">Home</a></li>
             <li><a href="#news">News</a></li>
             <li><a href="#contact">Contact</a></li>
             <li><a href="#about">About</a></li>
